1e6c0d3e824c7450aa0ee1c77616897a7ea4aa93
Henry Velasquez <hvelas93@gmail.com>
2021-02-03 01:58:01 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,5 +1,5 @@
 count = 0 # A bit of data defined outside the loop
-while count < 50 do # A Boolean expression using the bit of data
+while count < 51 do # A Boolean expression using the bit of data
   puts "I am the #{count}, I love to count!" # Work
   count = count + 1 # A bit of work that moves the bit of data closer to being false
 end
